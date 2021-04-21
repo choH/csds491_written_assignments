66029b1dc975c3377a962064381918d0453e6fb0
choH <hzproduction@hotmail.com>
2021-04-21 05:58:35 -0400

    🔨 ⚙️ Update code for E3 printouts.

--- A4/code/e3.py
+++ A4/code/e3.py
@@ -21,7 +21,7 @@ def my_PCA(input , n):
     first_n_eigenvectors = sorted_eigenvectors[:, 0 : n]
     reduced_input = np.dot(first_n_eigenvectors.transpose(), standardized_input.transpose()).transpose()
 
-    return reduced_input
+    return reduced_input, sorted_eigenvalue, sorted_eigenvectors
 
 
 
@@ -37,25 +37,36 @@ df = pd.read_csv('./A4/code/e3_glass.csv')
 label_column_name = 'Type'
 label = df.pop(label_column_name)
 
+print(df.head())
 print(df.shape)
 
 
 reduce_to_n = 2
 column_names = ['PC'+str(i) for i in range(1, reduce_to_n+1)]
-reduced_df = pd.DataFrame(my_PCA(df , reduce_to_n) , columns = column_names)
+reduced_df, sorted_eigenvalue, sorted_eigenvectors = my_PCA(df , reduce_to_n)
+reduced_df = pd.DataFrame(reduced_df, columns = column_names)
+reduced_label_df= pd.concat([reduced_df , pd.DataFrame(label)] , axis = 1)
 
-reduced_label_df = pd.concat([reduced_df , pd.DataFrame(label)] , axis = 1)
+# # 3.2.
+# print(sorted_eigenvalue)
+# print(sorted_eigenvectors)
+# print(f'<v_1, v_2> = {np.dot(sorted_eigenvectors[0], sorted_eigenvectors[1])}')
+# print(f'<v_2, v_3> = {np.dot(sorted_eigenvectors[1], sorted_eigenvectors[2])}')
+# print(f'<v_1, v_3> = {np.dot(sorted_eigenvectors[0], sorted_eigenvectors[2])}')
 
-# plt.figure(figsize = (10, 10))
-# sb.scatterplot(data = reduced_label_df, x = 'PC1', y = 'PC2', hue = label_column_name, s = 50, legend='full', palette= 'icefire')
 
-pca = PCA().fit(df)
-plt.plot(np.cumsum(pca.explained_variance_ratio_), color='b', label = 'cumulative')
-plt.plot(pca.explained_variance_ratio_, color='r', label = 'individual')
-plt.xlabel('number of components')
-plt.xticks([i for i in range(df.shape[1])])
-# plt.xticks([i for i in range(df.shape[1], 2)])
-plt.legend()
-plt.ylabel('explained variance');
+
+plt.figure(figsize = (10, 10))
+sb.scatterplot(data = reduced_label_df, x = 'PC1', y = 'PC2', hue = label_column_name, s = 50, legend='full', palette= 'icefire')
+
+# 3.3.
+# pca = PCA().fit(df)
+# plt.plot(np.cumsum(pca.explained_variance_ratio_), color='b', label = 'cumulative')
+# plt.plot(pca.explained_variance_ratio_, color='r', label = 'individual')
+# plt.xlabel('number of components')
+# plt.xticks([i for i in range(df.shape[1])])
+# # plt.xticks([i for i in range(df.shape[1], 2)])
+# plt.legend()
+# plt.ylabel('explained variance')
 
 plt.show()
